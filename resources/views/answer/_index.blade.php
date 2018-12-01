e669fca393f0b10c1c56e0b78ea75bf4f0afdeb5
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2018-12-01 07:50:30 -0800

    Hello World

---
+++ resources/views/answer/_index.blade.php
@@ -0,0 +1,31 @@
+<div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $answersCount." ". str_plural('Answer', $answersCount) }}</h2>
+                    </div>
+                    <hr>
+                    @foreach($answers as $answer)
+                        <div class="media">
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                                    <div class="media mt-2" >
+                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                                            <img src="{{ $answer->user->avatar }}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
